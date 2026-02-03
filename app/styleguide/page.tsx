32b7bcd60e7bf32848fad601e9176074b6cff729
Pedro Old <pedrohenrique.santos@corabiz.ag>
2026-02-03 17:02:38 -0300

    feat(text): create styleguide page and add testing text

---
+++ app/styleguide/page.tsx
@@ -0,0 +1,31 @@
+import { Text } from "@/src/components/atoms/Text/text";
+import styles from "./styleguide.module.scss";
+
+export default function documentationComponents() {
+    return (
+        <div className={styles.containerstyleguide}>
+            <section>
+                <h1>Component text - Atom</h1>
+                <p>Component de texto  base</p>
+                <div  className={styles.containerContentComponent}>
+                    <div className={styles.containerComponent}>
+                        <h3>Default text primary</h3>
+                        <Text isTitle={false} variant="primary">My text exemple primary default</Text>
+                    </div>
+                    <div className={styles.containerComponent}>
+                        <h3>Default text secondary</h3>
+                        <Text isTitle={false} variant="secondary">My text exemple secondary default</Text>
+                    </div>
+                    <div className={styles.containerComponent}>
+                        <h3>Title true primary</h3>
+                        <Text isTitle={true} variant="primary">My Title exemple primary</Text>
+                    </div>
+                    <div className={styles.containerComponent}>
+                        <h3>Title true secondary</h3>
+                        <Text isTitle={true} variant="secondary">My Title exemple secondary</Text>
+                    </div>
+                </div>
+            </section> 
+        </div>
+    )
+}
